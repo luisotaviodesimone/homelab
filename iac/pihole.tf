28d7196818e40030a832cffe9025579148c60f11
luisotaviodesimone <luisotaviodesimone@gmail.com>
2025-06-21 00:51:13 -0300

    feat: few more iac configuration

---
+++ iac/pihole.tf
@@ -0,0 +1,19 @@
+provider "pihole" {
+  url      = "https://pihole.lotds.duckdns.org" # PIHOLE_URL
+  password = var.pihole_password                # PIHOLE_PASSWORD
+}
+
+resource "pihole_dns_record" "ubuntu1" {
+  domain = "ubuntu1.lotds.duckdns.org"
+  ip     = "192.168.100.201"
+}
+
+resource "pihole_dns_record" "ubuntu2" {
+  domain = "ubuntu2.lotds.duckdns.org"
+  ip     = "192.168.100.202"
+}
+
+resource "pihole_dns_record" "ubuntu3" {
+  domain = "ubuntu3.lotds.duckdns.org"
+  ip     = "192.168.100.203"
+}
