3b57f42672376febaf32e0eaa7006b3cd3a7d48c
luisotaviodesimone <luisotaviodesimone@gmail.com>
2025-06-21 00:39:46 -0300

    feat: create minio backend

--- iac/main.tf
+++ iac/main.tf
@@ -4,15 +4,29 @@ terraform {
       source  = "iolave/pihole"
       version = "0.2.2-beta.2"
     }
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.2-rc01"
+    }
+    nginxproxymanager = {
+      source  = "home-devops/nginxproxymanager"
+      version = "1.1.3"
+    }
+  }
+  backend "s3" {
+    bucket  = "opentofu"
+    key     = "terraform.tfstate"
+
+    endpoints = {
+      s3 = "https://minioapi.lotds.duckdns.org"
+    }
+
+    region                      = "main"
+    skip_requesting_account_id  = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    use_path_style              = true
   }
 }
 
-provider "pihole" {
-  url      = "https://pihole.lotds.duckdns.org" # PIHOLE_URL
-  password = var.pihole_password                # PIHOLE_PASSWORD
-}
-
-resource "pihole_dns_record" "samba" {
-  domain = "samba.lotds.duckdns.org"
-  ip     = "192.168.100.200"
-}
